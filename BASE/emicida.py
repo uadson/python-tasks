9967a0af72e057608171b60cc987348d7aa05808
Uadson Feitosa <uadsonpy@gmail.com>
2023-02-08 11:30:44 -0300

    algoritmo de busca linear

--- BASE/emicida.py
+++ BASE/emicida.py
@@ -13,3 +13,23 @@
 # imports
 import os
 
+# 1. Obtendo o caminho exato da pasta em que o arquivo emicida.py está salvo
+
+caminho = os.getcwd()
+
+print(caminho)
+# saída: C:\Users\m913685\Documents\repos\stag-tasks\BASE
+
+# 2. Dentro do diretório BASE há uma pasta com o nome data na qual há um arquivo main.txt
+# buscando pelo pasta data
+for dirs, folders, files in os.walk(caminho):
+    # se em dirs houver uma pasta com nome data, percorrer esta pasta
+    # se em file existir algum com o nome de main.txt ele será salvo em uma variável de nome data
+    #print(dirs)
+    #print(folders)
+    #print(files)
+    for file in files:
+        if file == 'main.txt':
+            data = 'main.txt'
+    
+print(data)
